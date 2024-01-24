0ef362b96b4af7e9be5f463b34a246d89dbce010
Bhavya Raj Joshi <bhavyajoshi1617@gmail.com>
2024-01-24 21:03:22 +0530

    user controller final(history) and added routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -217,8 +217,7 @@ const refreshAccessToken = asyncHandler(async(req,res) => {
 })
 
 const changeCurrentPassword = asyncHandler(async(req, res) => {
-    const {oldPassword, newPassword} = req.body
-    
+
     const user = findById(req.user?._id)
 
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
@@ -413,6 +412,55 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
     )
 })
 
+const getWatchHistory = asyncHandler(async(req,res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users" ,
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $projects: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1   
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    $addFields: {
+                        owner: {
+                            $first: "$owner"
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200 , user[0].watchHistory, "watch history fetched successfully")
+    )
+
+})
+
 export {
     registerUser,
     loginUser,
@@ -422,5 +470,7 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateAvatar,
-    updateCoverImage
+    updateCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,19 +5,33 @@ import { refreshAccessToken } from "../controllers/user.controller.js";
 import { loginUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { logoutUser } from "../controllers/user.controller.js";
+import{
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateAvatar,
+    updateCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js"
  
 const router = Router()
 
 
-router.route("/register").post(
-    upload.fields([{name: 'avatar', maxCount: 1},{name: 'coverImage', maxCount: 1}]) ,
-    registerUser)
-
+router.route("/register").post(upload.fields([{name: 'avatar', maxCount: 1},{name: 'coverImage', maxCount: 1}]) , registerUser)
 router.route("/login").post(loginUser)
-
 router.route("/logout").post(verifyJWT, logoutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/get-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"),  updateAvatar)
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"),  updateCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+
 
 export default router
 
