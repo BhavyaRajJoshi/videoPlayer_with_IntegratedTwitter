54089001cde8e94795f7abba2c923eda09644b82
Bhavya Raj Joshi <bhavyajoshi1617@gmail.com>
2024-01-29 14:38:39 +0530

    checking env

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -13,7 +13,6 @@ const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
     /*
-
     */
 })
 
@@ -84,6 +83,8 @@ const getVideoById = asyncHandler(async (req, res) => {
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+
+    
 
 })
 
