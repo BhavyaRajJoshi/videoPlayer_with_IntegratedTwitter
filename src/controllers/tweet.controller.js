4a8d5687c8a0cac41788c7916bfcc48a31b04e4b
Bhavya Raj Joshi <bhavyarajjoshiatwork@gmail.com>
2024-02-06 19:42:35 +0530

    completed tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -35,14 +35,55 @@ const createTweet = asyncHandler(async (req, res) => {
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    //const user = User.findById(req.user?._id)
+
+    const userTweets = await Tweet.find({owner: req.user?._id})
+
+    res.status(200)
+    .json(new ApiResponse(200, userTweets, "user tweets export successfully."))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+
+    try {
+        const { newContent } = req.body;
+    
+        // Check if newContent is provided
+        if (!newContent) {
+          throw new ApiError(400, "New content is required for updating a tweet");
+        }
+    
+        // Update tweets based on the owner's ID
+        const updatedTweets = await Tweet.updateMany(
+          { owner: req.user?._id },
+          { $set: 
+            { content: newContent }
+        }, {new : true}
+        );
+    
+        res.status(200)
+        .json(new ApiResponse(200, updatedTweets, "Tweets updated successfully"));
+      } catch (error) {
+
+        throw new ApiError(200, error, "cannot update tweet")
+      }
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+
+    const tweetId = req.params.tweetId
+
+    if(!tweetId){
+        throw new ApiError(200, "cannot get the tweet to delete")
+    }
+
+    await findByIdAndDelete(tweetId)
+
+    res.status(200)
+    .json(200, "tweet deleted successfully")
+
 })
 
 export {
